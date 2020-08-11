fb34572cdabb2caa53cc32a69415572fc73d8006
Beardocracy <travisjbearden@gmail.com>
2020-08-11 14:32:55 -0500

    Added player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
+    public float speed = 200f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("It works!");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }      
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("w") || Input.GetKey("up"))
+        {
+            rb.AddForce(0, 0, speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s") || Input.GetKey("down"))
+        {
+            rb.AddForce(0, 0, -speed * Time.deltaTime);
+        }
     }
 }
